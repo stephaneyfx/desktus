fc30d5d3f470212491c8d515c8b2c3b67465c337
Stephane Raux <stephaneyfx@gmail.com>
2021-03-04 01:41:23 -0800

    Add NIC stats and refactor

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,28 @@
 
 #![deny(warnings)]
 
-use async_ctrlc::CtrlC;
-use async_std::prelude::FutureExt as _;
 use chrono::Local;
-use futures::{Stream, StreamExt, TryFutureExt};
+use futures::{
+    future::ready,
+    FutureExt, pin_mut, Stream, StreamExt,
+};
+use number_prefix::NumberPrefix;
 use palette::Srgb;
 use serde::{Serialize, Serializer};
-use std::time::{Duration, Instant};
+use std::{
+    collections::HashMap,
+    fmt::{self, Display},
+    ops::{Add, AddAssign, Sub},
+    time::{Duration, Instant},
+};
 
-fn main() {
-    let ctrlc = CtrlC::new().expect("Failed to register CTRL+C handler");
+#[tokio::main]
+async fn main() {
     println!(r#"{{ "version": 1 }}"#);
     println!("[");
-    async_std::task::block_on(write_blocks().race(ctrlc));
+    let ctrl_c = tokio::signal::ctrl_c();
+    pin_mut!(ctrl_c);
+    futures::future::select(write_blocks().boxed(), ctrl_c).await;
     println!("[]");
     println!("]");
 }
@@ -70,6 +79,7 @@ where
 
 async fn write_blocks() {
     let streams = vec![
+        render_nic_info().boxed(),
         render_disk_usage().boxed(),
         render_memory_usage().boxed(),
         render_cpu_usage().boxed(),
@@ -99,21 +109,19 @@ fn render_blocks(blocks: &[Vec<Block>]) {
 }
 
 fn render_time() -> impl Stream<Item = Vec<Block>> {
-    async_std::stream::once(())
-        .chain(async_std::stream::interval(Duration::from_secs(1)))
-        .map(|_| {
-            let t = Local::now();
-            vec![
-                Block::new(t.format("%a %d %b %Y").to_string()),
-                Block::new(t.format("%R").to_string()),
-            ]
-        })
+    let blocks = || {
+        let t = Local::now();
+        vec![
+            Block::new(t.format("%a %d %b %Y").to_string()),
+            Block::new(t.format("%R").to_string()),
+        ]
+    };
+    throttle(Duration::from_secs(1), futures::stream::repeat_with(blocks))
 }
 
 fn render_battery() -> impl Stream<Item = Vec<Block>> {
-    async_std::stream::once(())
-        .chain(async_std::stream::interval(Duration::from_secs(60)))
-        .map(|_| make_battery_block().ok().flatten().map_or(Vec::new(), |b| vec![b]))
+    let blocks = || make_battery_block().ok().flatten().into_iter().collect();
+    throttle(Duration::from_secs(60), futures::stream::repeat_with(blocks))
 }
 
 fn make_battery_block() -> Result<Option<Block>, battery::Error> {
@@ -159,25 +167,22 @@ fn battery_icon(capacity: f32, charging: bool) -> char {
 }
 
 fn render_cpu_usage() -> impl Stream<Item = Vec<Block>> {
-    heim::cpu::time()
-        .map_ok(|stats| {
-            let stats = CpuTime::from(stats);
-            futures::stream::try_unfold(stats, |old_stats| async move {
-                let new_stats = CpuTime::from(heim::cpu::time().await?);
-                Ok(Some((old_stats.usage(&new_stats), new_stats)))
-            })
-        })
-        .try_flatten_stream()
-        .map(|usage| usage.map_or(Vec::new(), |u| vec![cpu_usage_block(u)]))
-        .zip(
-            async_std::stream::once(())
-                .chain(async_std::stream::interval(Duration::from_secs(5)))
-        )
-        .map(|(u, _)| u)
+    let blocks = futures::stream::repeat(())
+        .then(|_| heim::cpu::time())
+        .scan(None::<CpuTime>, |old_stats, new_stats| {
+            let new_stats = new_stats.ok().map(CpuTime::from);
+            let blocks = match (&*old_stats, &new_stats) {
+                (Some(old), Some(new)) => vec![cpu_usage_block(old.usage(new))],
+                _ => Vec::new(),
+            };
+            *old_stats = new_stats;
+            ready(Some(blocks))
+        });
+    throttle(Duration::from_secs(5), blocks)
 }
 
 fn cpu_usage_block(usage: u32) -> Block {
-    let block = Block::new(format!("\u{f061a} {}%", usage));
+    let block = Block::new(format!("\u{f08d6} {}%", usage));
     block.critical_if(usage >= 95)
 }
 
@@ -210,55 +215,244 @@ impl From<heim::cpu::CpuTime> for CpuTime {
 }
 
 fn render_memory_usage() -> impl Stream<Item = Vec<Block>> {
-    async_std::stream::once(())
-        .chain(async_std::stream::interval(Duration::from_secs(30)))
+    let blocks = futures::stream::repeat(())
         .then(|_| async {
-            let mem_stats = heim::memory::memory().await.ok()?;
-            Some(memory_usage_block(mem_stats))
+            heim::memory::memory()
+                .await
+                .ok()
+                .map(memory_usage_block)
+                .into_iter()
+                .collect()
         })
-        .map(|usage| usage.map_or(Vec::new(), |u| vec![u]))
+        .boxed();
+    throttle(Duration::from_secs(5), blocks)
 }
 
 fn memory_usage_block(mem_stats: heim::memory::Memory) -> Block {
-    let total = gibibytes(mem_stats.total());
-    let available = gibibytes(mem_stats.available());
+    let total = bytes(mem_stats.total());
+    let available = bytes(mem_stats.available());
     let used = total - available;
-    let block = Block::new(format!("\u{f035b} {:.2} / {:.2} GiB", used, total));
+    let (used, total) = (used as f64, total as f64);
+    let block = Block::new(format!("\u{f035b} {} / {}", pretty_bytes(used), pretty_bytes(total)));
     block.critical_if(used / total >= 0.95)
 }
 
 fn render_disk_usage() -> impl Stream<Item = Vec<Block>> {
-    async_std::stream::once(())
-        .chain(async_std::stream::interval(Duration::from_secs(120)))
+    let blocks = futures::stream::repeat(())
         .then(|_| async {
             let (used, total) = heim::disk::partitions_physical()
                 .filter_map(|partition| async move {
                     let usage = heim::disk::usage(partition.ok()?.mount_point()).await.ok()?;
-                    Some((mebibytes_nat(usage.used()), mebibytes_nat(usage.total())))
+                    Some((bytes(usage.used()), bytes(usage.total())))
                 })
                 .fold((0u64, 0u64), |(acc_used, acc_total), (used, total)| async move {
                     (acc_used + used, acc_total + total)
                 })
                 .await;
-            vec![disk_usage_block(used as f64 / 1000.0, total as f64 / 1000.0)]
+            vec![disk_usage_block(used as f64, total as f64)]
         })
+        .boxed();
+    throttle(Duration::from_secs(60), blocks)
 }
 
 fn disk_usage_block(used: f64, total: f64) -> Block {
-    Block::new(format!("\u{f01bc} {:.2} / {:.2} GiB", used, total))
+    Block::new(format!("\u{f01bc} {} / {}", pretty_bytes(used), pretty_bytes(total)))
         .critical_if(used / total >= 0.9)
+}
+
+fn render_nic_info() -> impl Stream<Item = Vec<Block>> {
+    let blocks = NetIoSnapshot::capture()
+        .map(|snapshot| futures::stream::unfold(snapshot, |old_snapshot| async move {
+            let new_snapshot = NetIoSnapshot::capture().await;
+            let diff = new_snapshot.stats - old_snapshot.stats;
+            let elapsed = new_snapshot.time - old_snapshot.time;
+            Some((net_io_blocks(diff, elapsed), new_snapshot))
+        }))
+        .flatten_stream()
+        .boxed();
+    throttle(Duration::from_secs(5), blocks)
+}
+
+fn net_io_blocks(stats: NetIoStats, elapsed: Duration) -> Vec<Block> {
+    vec![
+        bytes_per_second_block("\u{f01da}", stats.external.bytes_read, elapsed),
+        bytes_per_second_block("\u{f0552}", stats.external.bytes_written, elapsed),
+        bytes_per_second_block("\u{f1320}", stats.loopback.bytes_read, elapsed),
+        bytes_per_second_block("\u{f1373}", stats.loopback.bytes_written, elapsed),
+    ]
+}
+
+fn bytes_per_second_block(icon: &str, byte_count: u64, elapsed: Duration) -> Block {
+    let elapsed = elapsed.as_secs_f64();
+    let count = byte_count as f64 / elapsed;
+    Block::new(format!("{} {}/s", icon, pretty_bytes(count)))
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+struct IoStats {
+    bytes_written: u64,
+    bytes_read: u64,
+}
+
+impl IoStats {
+    fn from_net(counters: &heim::net::IoCounters) -> Self {
+        Self {
+            bytes_written: bytes(counters.bytes_sent()),
+            bytes_read: bytes(counters.bytes_recv()),
+        }
+    }
+}
+
+impl Add for IoStats {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            bytes_written: self.bytes_written + rhs.bytes_written,
+            bytes_read: self.bytes_read + rhs.bytes_read,
+        }
+    }
+}
+
+impl Sub for IoStats {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            bytes_written: self.bytes_written - rhs.bytes_written,
+            bytes_read: self.bytes_read - rhs.bytes_read,
+        }
+    }
+}
+
+impl AddAssign for IoStats {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+struct NetIoStats {
+    loopback: IoStats,
+    external: IoStats,
+}
+
+impl Add for NetIoStats {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            loopback: self.loopback + rhs.loopback,
+            external: self.external + rhs.external,
+        }
+    }
+}
+
+impl Sub for NetIoStats {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            loopback: self.loopback - rhs.loopback,
+            external: self.external - rhs.external,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct NetIoSnapshot {
+    stats: NetIoStats,
+    time: Instant,
+}
+
+impl NetIoSnapshot {
+    async fn capture() -> Self {
+        let nics = heim::net::nic()
+            .filter_map(|nic| async {
+                nic.ok().filter(|nic| nic.is_up()).map(|nic| {
+                    (nic.name().to_owned(), nic.is_loopback())
+                })
+            })
+            .collect::<HashMap<_, _>>()
+            .await;
+        heim::net::io_counters()
+            .filter_map(|counters| async { counters.ok() })
+            .fold(NetIoStats::default(), move |mut acc, counters| {
+                match nics.get(counters.interface()) {
+                    Some(true) => acc.loopback += IoStats::from_net(&counters),
+                    Some(false) => acc.external += IoStats::from_net(&counters),
+                    None => {}
+                }
+                async move { acc }
+            })
+            .map(|stats| Self { stats, time: Instant::now() })
+            .await
+    }
+}
+
+#[derive(Clone, Debug)]
+struct PrettyQuantity {
+    quantity: f64,
+}
+
+impl From<f64> for PrettyQuantity {
+    fn from(quantity: f64) -> Self {
+        Self { quantity }
+    }
+}
+
+impl Display for PrettyQuantity {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match NumberPrefix::decimal(self.quantity) {
+            NumberPrefix::Standalone(q) => write!(f, "{:.1} ", q),
+            NumberPrefix::Prefixed(prefix, q) => write!(f, "{:.1} {}", q, prefix),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct WithUnit {
+    value: PrettyQuantity,
+    unit: &'static str,
+}
+
+impl WithUnit {
+    fn new(q: f64, unit: &'static str) -> Self {
+        Self {
+            value: q.into(),
+            unit,
+        }
+    }
+}
+
+impl Display for WithUnit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.value, self.unit)
+    }
+}
+
+fn pretty_bytes(n: f64) -> WithUnit {
+    WithUnit::new(n, "B")
 }
 
 fn duration(t: heim::units::Time) -> Duration {
     Duration::from_nanos(t.get::<heim::units::time::nanosecond>() as u64)
 }
 
-fn mebibytes_nat(q: heim::units::Information) -> u64 {
-    q.get::<heim::units::information::mebibyte>()
-}
-
-fn gibibytes(q: heim::units::Information) -> f64 {
-    q.get::<heim::units::information::mebibyte>() as f64 / 1000.0
+fn bytes(q: heim::units::Information) -> u64 {
+    q.get::<heim::units::information::byte>()
 }
 
 const CRITICAL_COLOR: Srgb<u8> = palette::named::FIREBRICK;
+
+fn throttle<S>(period: Duration, stream: S) -> impl Stream<Item = S::Item>
+where
+    S: Stream + Unpin,
+{
+    let interval = tokio::time::interval(period);
+    futures::stream::unfold((interval, stream), |(mut interval, mut stream)| async move {
+        interval.tick().await;
+        let item = stream.next().await;
+        item.map(|item| (item, (interval, stream)))
+    })
+}
