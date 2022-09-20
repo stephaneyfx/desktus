930756260e0e1619ff94b64dea12f4661dfc1400
Stephane Raux <stephaneyfx@gmail.com>
2022-09-20 00:07:58 -0500

    Update dependencies

--- src/sources/cpu.rs
+++ src/sources/cpu.rs
@@ -1,9 +1,9 @@
 // Copyright (C) 2019-2022 Stephane Raux. Distributed under the 0BSD license.
 
-use sysinfo::{ProcessorExt, System, SystemExt};
+use sysinfo::{CpuExt, System, SystemExt};
 
 pub fn cpu_usage(system: &mut System) -> u32 {
     system.refresh_cpu();
-    let usage = system.global_processor_info().cpu_usage();
+    let usage = system.global_cpu_info().cpu_usage();
     100.min(usage.round() as u32)
 }
